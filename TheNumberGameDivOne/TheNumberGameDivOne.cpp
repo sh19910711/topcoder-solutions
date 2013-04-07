cbd467cf30f5f63d7db8068fa7c843dc70ee6708
Hiroyuki Sano <sh19910711@gmail.com>
2013-04-07 14:09:52 +0900

    SRM 575 DIV 1 Easy - TheNumberGameDivOne: Accepted

---
+++ TheNumberGameDivOne/TheNumberGameDivOne.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <limits>
+#include <complex>
+#include <functional>
+#include <iterator>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+struct TheNumberGameDivOne { string find( long long n ); };
+
+// @snippet<sh19910711/contest:solution/typedef.cpp>
+namespace solution {
+    typedef std::istringstream ISS;
+    typedef std::ostringstream OSS;
+    typedef std::vector<std::string> VS;
+    typedef long long LL;
+    typedef int INT;
+    typedef std::vector<INT> VI;
+    typedef std::vector<VI> VVI;
+    typedef std::pair<INT,INT> II;
+    typedef std::vector<II> VII;
+}
+
+namespace solution {
+    const string YES = "John";
+    const string NO = "Brus";
+    struct Solution {
+        string solve( long long n ) {
+            if ( n % 2 )
+                return NO;
+            int p = 1;
+            while ( n >= 2 && n % 2 == 0 ) {
+                p ^= 1;
+                n /= 2;
+            }
+            if ( n == 1 && ! p )
+                return NO;
+            return YES;
+        }
+    };
+
+}
+
+string TheNumberGameDivOne::find( long long n ) {
+    return solution::Solution().solve(n);
+}
+
+// BEGIN CUT HERE
+// BEGIN CUT HERE
+#include <ctime>
+#include <sstream>
+#include <fstream>
+double start_time; string timer()
+{ ostringstream os; os << " (" << int((clock()-start_time)/CLOCKS_PER_SEC*1000) << " msec)"; return os.str(); }
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v)
+{ os << "{ ";
+    for(typename vector<T>::const_iterator it=v.begin(); it!=v.end(); ++it)
+        os << '\"' << *it << '\"' << (it+1==v.end() ? "" : ", "); os << " }"; return os; }
+        void verify_case(const int caseno, const long long&n, const string& Expected, bool verbose = false) {
+            string Received = TheNumberGameDivOne().find(n);
+            cerr << "Test Case #" << caseno << "...";
+            bool ok = (Expected == Received);
+            if(ok) cerr << "PASSED" << timer() << endl;   else { cerr << "FAILED" << timer() << endl;
+                if (verbose) cerr << "\tn: " << n<< endl;
+                cerr << "\to: \"" << Expected << '\"' << endl << "\tx: \"" << Received << '\"' << endl; } }
+                void notify_memory_usage(){
+#ifndef _WIN32
+                    std::ifstream ifs("/proc/self/status",std::ios_base::in);
+                    std::string str;
+                    for(;;){std::getline(ifs, str);
+                        if(str.find("VmPeak") != std::string::npos){cout << str << " (< 64MB)" << endl;}
+                        if(str.find("VmStk") != std::string::npos){cout << str << " (< 8MB)" << endl;break;} }
+#endif
+                }
+#define CASE(N) if (N==runno_ || (runno_<0 && N+1>=-runno_)) {int caseno=N; start_time=clock();
+#define RUN_TEST()	 verify_case(caseno, n, _, verbose_);}
+int main(int argc, char **argv){
+    bool verbose_ = false;
+    int runno_ = -1;
+    if (argc >= 2) if(!strcmp(argv[1], "-v")) verbose_ = true;
+    if (argc == 2 && !verbose_) runno_ = atoi(argv[1]);
+    else if (argc == 3 && verbose_) runno_ = atoi(argv[2]);
+    
+    CASE(0){
+        long long n = 6LL;
+        string _ = "John";
+        RUN_TEST();
+    }
+    CASE(1){
+        long long n = 2LL;
+        string _ = "Brus";
+        RUN_TEST();
+    }
+    CASE(2){
+        long long n = 747LL;
+        string _ = "Brus";
+        RUN_TEST();
+    }
+    CASE(3){
+        long long n = 128LL;
+        string _ = "Brus";
+        RUN_TEST();
+    }
+    notify_memory_usage();
+    
+}
+// END CUT HERE
+// END CUT HERE
+
